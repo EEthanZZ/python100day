c73bd1b421d34046fde9a9b581bb3ebc17880ce1
EEthanZZ <yczhu@yahoo.com>
2022-09-22 09:51:23 +1000

    code complete

--- D3_odd_or_even.py
+++ D3_odd_or_even.py
@@ -1,1 +1,5 @@
-number = int(input("ennter a number here:"))
+number = int(input("ennter a number here: "))
+if number % 2 == 0:
+    print("it's an even nunmber")
+else:
+    print("it's an add number")
