7c4f8bd0592f58bfd51f847c1e9514901ec8efa7
EEthanZZ <yczhu@yahoo.com>
2022-09-22 10:39:07 +1000

    size price complete

--- D3_pizzza_order.py
+++ D3_pizzza_order.py
@@ -13,3 +13,13 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
+
+price = 0
+if size == 'S':
+    price += 15
+if size == 'M':
+    price += 20
+if size == 'L':
+    price += 25
+
+print(f'Your order is ${price} in total.')
