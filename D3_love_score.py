b12e50c580267d79b182f90d2df0be4d3947ed3c
EEthanZZ <yczhu@yahoo.com>
2022-09-22 10:58:05 +1000

    "true" value

--- D3_love_score.py
+++ D3_love_score.py
@@ -1,5 +1,15 @@
 # 🚨 Don't change the code below 👇
+from tkinter import E
+
+
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
+#name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
+
+t_count = int(name1.lower().count('t'))
+r_count = int(name1.lower().count('r'))
+u_count = int(name1.lower().count('u'))
+e_count = int(name1.lower().count('e'))
+
+true_count = (t_count+r_count+u_count+e_count) * 10
