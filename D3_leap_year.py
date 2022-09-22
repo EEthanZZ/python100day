d45c3e1b55b8cac79607d78f2a7658c1f260311e
EEthanZZ <yczhu@yahoo.com>
2022-09-22 10:15:54 +1000

    not "leap year" print not included

--- D3_leap_year.py
+++ D3_leap_year.py
@@ -4,3 +4,7 @@
 # **unless** the year is also evenly divisible by 400
 
 year = int(input("Which year do you want to check? "))
+if year % 4 == 0:
+    if year % 100 != 0:
+        if year % 400 == 0:
+            print("it is a leap year")
