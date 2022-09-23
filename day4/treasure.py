7ad697269739ea99159e0bd6dee620ba69fbc254
EEthanZZ <yczhu@yahoo.com>
2022-09-23 15:30:42 +1000

    infinity loop

--- day4/treasure.py
+++ day4/treasure.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import random
 from re import M
 
@@ -7,18 +8,24 @@ row3 = ["⬜️", "⬜️", "⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 
-random_x = random.randint(0, 2)
-random_y = random.randint(0, 2)
+random_x = str(random.randint(0, 2))
+random_y = str(random.randint(0, 2))
+random_xy = random_x + random_y
+print(random_xy)
 
 
-position = input("Where do you want to put the treasure? ")
+game = True
+while game:
+    position = input("Where do you want to put the treasure? ")
+    list = []
+    for i in position:
+        list.append(int(i))
 
-list = []
-for i in position:
-    list.append(int(i))
+    map[list[0] - 1][list[1] - 1] = "X "
 
-print(list)
-
-map[list[0] - 1][list[1] - 1] = "X "
-
-print(f"{row1}\n{row2}\n{row3}")
+    print(f"{row1}\n{row2}\n{row3}")
+    if random_xy == position:
+        print("you win")
+        game == False
+    else:
+        print("try again")
