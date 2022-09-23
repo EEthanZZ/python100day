b0bd928291a15e5d032e9be3b2d9f0995a004ed9
EEthanZZ <yczhu@yahoo.com>
2022-09-23 14:53:33 +1000

    for loop to split posttion, locate X in the map

--- day4/treasure.py
+++ day4/treasure.py
@@ -7,9 +7,13 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
+list = []
+for i in position:
+    list.append(int(i))
 
-put_x = random.randint(0, 2)
-put_y = random.randint(0, 2)
-map[put_x][put_y] = "X"
+print(list)
+
+map[list[0] - 1][list[1] - 1] = "X "
+
 
 print(f"{row1}\n{row2}\n{row3}")
