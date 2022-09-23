9edac28d7106ff638ffaa1a68ce996176e5c1430
EEthanZZ <yczhu@yahoo.com>
2022-09-23 14:35:31 +1000

    "x" palced in the map

--- day4/treasure.py
+++ day4/treasure.py
@@ -1,3 +1,5 @@
+import random
+
 row1 = ["⬜️", "⬜️", "⬜️"]
 row2 = ["⬜️", "⬜️", "⬜️"]
 row3 = ["⬜️", "⬜️", "⬜️"]
@@ -6,6 +8,8 @@ print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
 
-
+put_x = random.randint(0, 2)
+put_y = random.randint(0, 2)
+map[put_x][put_y] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
