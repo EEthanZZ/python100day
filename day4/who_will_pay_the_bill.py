68ddfdf1b7c431dfb0bf531805e4868cc440c204
EEthanZZ <yczhu@yahoo.com>
2022-09-23 14:17:47 +1000

    random by choice

--- day4/who_will_pay_the_bill.py
+++ day4/who_will_pay_the_bill.py
@@ -3,6 +3,6 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(",")
 
-random_person = random.randint(0, len(names) - 1)
-pay = names[random_person]
-print(pay)
+# random_person = random.randint(0, len(names) - 1)
+person = random.choice(names)
+print(person)
