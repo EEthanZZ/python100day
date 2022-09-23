a1aa627b29ec07870aafdc269e5d34d60863f6f1
EEthanZZ <yczhu@yahoo.com>
2022-09-23 14:15:40 +1000

    compelete, using randint

--- day4/who_will_pay_the_bill.py
+++ day4/who_will_pay_the_bill.py
@@ -3,4 +3,6 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(",")
 
-print(names)
+random_person = random.randint(0, len(names) - 1)
+pay = names[random_person]
+print(pay)
