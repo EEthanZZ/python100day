badcbed88b0b4c2682b94e8ed12bbdbab9a80c26
EEthanZZ <yczhu@yahoo.com>
2022-09-23 15:15:19 +1000

    create a random x,y position

--- day4/treasure.py
+++ day4/treasure.py
@@ -1,10 +1,16 @@
 import random
+from re import M
 
 row1 = ["⬜️", "⬜️", "⬜️"]
 row2 = ["⬜️", "⬜️", "⬜️"]
 row3 = ["⬜️", "⬜️", "⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
+
+random_x = random.randint(0, 2)
+random_y = random.randint(0, 2)
+
+
 position = input("Where do you want to put the treasure? ")
 
 list = []
@@ -14,6 +20,5 @@ for i in position:
 print(list)
 
 map[list[0] - 1][list[1] - 1] = "X "
-
 
 print(f"{row1}\n{row2}\n{row3}")
