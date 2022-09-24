9793f6c297341e98b90241dd369e035107392438
EEthanZZ <yczhu@yahoo.com>
2022-09-25 00:33:28 +1000

    highest score practice

--- day5/average_height.py
+++ day5/average_height.py
@@ -1,12 +1,17 @@
-stu_height = input("input a list of student height").split()
-sum = 0
-for i in range(0, len(stu_height)):
-    x = int(stu_height[i])
-    sum += x
-print(stu_height)
-print(sum)
-ave = int(sum / len(stu_height))
-print(ave)
+# stu_height = input("input a list of student height").split()
+# sum = 0
+# for i in range(0, len(stu_height)):
+#     x = int(stu_height[i])
+#     sum += x
+# print(stu_height)
+# print(sum)
+# ave = int(sum / len(stu_height))
+# print(ave)
 
 
 high_score = input("input a list of student scores").split()
+highest = 0
+for i in high_score:
+    if int(i) > int(highest):
+        highest == i
+print(highest)
