bfe1541c3787810509f4918858e2785dee3d54f2
EEthanZZ <yczhu@yahoo.com>
2022-09-25 01:37:13 +1000

    hard level method 1 done

--- day5/random_pass.py
+++ day5/random_pass.py
@@ -13,15 +13,19 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 # Eazy Level - Order not randomised:
 # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-letter_pass = []
-for i in range(0, nr_letters):
-    letter_pass.append(random.choice(letters))
-for i in range(0, nr_symbols):
-    letter_pass.append(random.choice(symbols))
-for i in range(0, nr_numbers):
-    letter_pass.append(random.choice(numbers))
-print(letter_pass)
+# letter_pass = []
+# for i in range(0, nr_letters):
+#     letter_pass.append(random.choice(letters))
+# for i in range(0, nr_symbols):
+#     letter_pass.append(random.choice(symbols))
+# for i in range(0, nr_numbers):
+#     letter_pass.append(random.choice(numbers))
+# print(letter_pass)
 
 # Hard Level - Order of characters randomised:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+pass_2 = []
 all_pass = letters + numbers + symbols
+for i in range(0, nr_letters+nr_numbers+nr_symbols):
+    pass_2.append(random.choice(all_pass))
+print(pass_2)
