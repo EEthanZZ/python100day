509a0a042231a29c3a674c64bd8e265ac0ee1353
EEthanZZ <yczhu@yahoo.com>
2022-09-25 00:47:33 +1000

    enable both 3 methods

--- day5/sum_of_even_num.py
+++ day5/sum_of_even_num.py
@@ -1,16 +1,16 @@
 
 
-# sum = 0
-# for i in range(0, 101):
-#     if i % 2 == 0:
-#         sum += i
-# print(sum)
+sum = 0
+for i in range(0, 101):
+    if i % 2 == 0:
+        sum += i
+print(sum)
 
 
-# sum2 = 0
-# for i in range(0, 101, 2):
-#     sum2 += i
-# print(sum2)
+sum2 = 0
+for i in range(0, 101, 2):
+    sum2 += i
+print(sum2)
 
 b = 0
 a = 0
