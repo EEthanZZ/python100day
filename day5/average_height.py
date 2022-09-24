0146af01dab6b07311b50679db9219079227ea03
EEthanZZ <yczhu@yahoo.com>
2022-09-25 00:20:48 +1000

    practise completed

---
+++ day5/average_height.py
@@ -0,0 +1,9 @@
+stu_height = input("input a list of student height").split()
+sum = 0
+for i in range(0, len(stu_height)):
+    x = int(stu_height[i])
+    sum += x
+print(stu_height)
+print(sum)
+ave = int(sum / len(stu_height))
+print(ave)
