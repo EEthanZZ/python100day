e316bbc71a70035c99ba352c3dcfaf4df071a802
EEthanZZ <yczhu@yahoo.com>
2022-09-25 00:47:04 +1000

    method 3, using while loop!!

--- day5/sum_of_even_num.py
+++ day5/sum_of_even_num.py
@@ -1,11 +1,20 @@
-sum = 0
-for i in range(0, 101):
-    if i % 2 == 0:
-        sum += i
-print(sum)
 
 
-sum2 = 0
-for i in range(0, 101, 2):
-    sum2 += i
-print(sum2)
+# sum = 0
+# for i in range(0, 101):
+#     if i % 2 == 0:
+#         sum += i
+# print(sum)
+
+
+# sum2 = 0
+# for i in range(0, 101, 2):
+#     sum2 += i
+# print(sum2)
+
+b = 0
+a = 0
+while b < 100:
+    b += 2
+    a += b
+print(a)
