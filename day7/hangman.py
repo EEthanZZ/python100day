2b3354263d05ebbccbb1b39652599f960ab79141
EEthanZZ <yczhu@yahoo.com>
2022-09-26 16:12:24 +1000

    bug fixed, now can repalce the _ with the input

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,4 +1,5 @@
 import random
+from turtle import pen
 # create the word list and guess the letter from the selected word.
 word_list = ["apple", "banana", "orange"]
 x = input("guess the letter")
@@ -8,6 +9,14 @@ print(f"the word chosen is {select_word}")
 
 z = list(select_word)
 print(z)
-for i in range(len(z)):
-    z[i] = '_'
+# for i in range(len(z)):
+#     z[i] = '_'
+# print(z)
+a = 0
+for i in z:
+    if x == i:
+        z[a] = x
+    else:
+        z[a] = "_"
+    a += 1
 print(z)
