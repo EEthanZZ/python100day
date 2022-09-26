cb8ec28a83f6093c10f71aa99acd42ee7bb3af07
EEthanZZ <yczhu@yahoo.com>
2022-09-26 20:48:42 +1000

    recode to allow letter replace "_" by inputs

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,22 +1,24 @@
+
 import random
-from turtle import pen
 # create the word list and guess the letter from the selected word.
 word_list = ["apple", "banana", "orange"]
-guess_letter = input("guess the letter")
-select_word = word_list[random.randint(0, len(word_list)-1)]
 
+select_word = random.choice(word_list)
+word_len = len(select_word)
 print(f"the word chosen is {select_word}")
 
-word_into_leeter = list(select_word)
-print(word_into_leeter)
-# for i in range(len(z)):
-#     z[i] = '_'
-# print(z)
-a = 0
-for word in word_into_leeter:
-    if guess_letter == word:
-        word_into_leeter[a] = guess_letter
-    else:
-        word_into_leeter[a] = "_"
-    a += 1
-print(word_into_leeter)
+show = []
+for a in range(word_len):
+    show += "_"
+
+game = True
+while game:
+    guess_letter = input("guess the letter: ").lower()
+    for x in range(word_len):
+        letter = select_word[x]
+        if letter == guess_letter:
+            show[x] = letter
+    print(show)
+    if "_" not in show:
+        game = False
+        print("you win")
