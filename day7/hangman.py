edbf581ace843bc32c286ab0c7708afefc1d74c3
EEthanZZ <yczhu@yahoo.com>
2022-09-26 16:15:04 +1000

    rename the varaibles

--- day7/hangman.py
+++ day7/hangman.py
@@ -2,21 +2,21 @@ import random
 from turtle import pen
 # create the word list and guess the letter from the selected word.
 word_list = ["apple", "banana", "orange"]
-x = input("guess the letter")
+guess_letter = input("guess the letter")
 select_word = word_list[random.randint(0, len(word_list)-1)]
 
 print(f"the word chosen is {select_word}")
 
-z = list(select_word)
-print(z)
+word_into_leeter = list(select_word)
+print(word_into_leeter)
 # for i in range(len(z)):
 #     z[i] = '_'
 # print(z)
 a = 0
-for i in z:
-    if x == i:
-        z[a] = x
+for word in word_into_leeter:
+    if guess_letter == word:
+        word_into_leeter[a] = guess_letter
     else:
-        z[a] = "_"
+        word_into_leeter[a] = "_"
     a += 1
-print(z)
+print(word_into_leeter)
