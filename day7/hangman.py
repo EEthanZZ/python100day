192098df6963a69a1ca9bca91614ece1041ded15
EEthanZZ <yczhu@yahoo.com>
2022-09-26 15:52:40 +1000

    split the word and replace the letters with _

--- day7/hangman.py
+++ day7/hangman.py
@@ -2,4 +2,12 @@ import random
 # create the word list and guess the letter from the selected word.
 word_list = ["apple", "banana", "orange"]
 x = input("guess the letter")
-select_word = word_list[random.randint(0, len(word_list))]
+select_word = word_list[random.randint(0, len(word_list)-1)]
+
+print(f"the word chosen is {select_word}")
+
+z = list(select_word)
+print(z)
+for i in range(len(z)):
+    z[i] = '_'
+print(z)
