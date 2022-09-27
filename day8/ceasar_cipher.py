eb06acdc128b0bab29d337be98338fb34f5eac5c
EEthanZZ <yczhu@yahoo.com>
2022-09-27 23:31:14 +1000

    combine the 3 functions into a single function

--- day8/ceasar_cipher.py
+++ day8/ceasar_cipher.py
@@ -9,33 +9,48 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(shift_amount, text_input):
+# def encrypt(shift_amount, text_input):
+#     new_cipher = ''
+#     for i in text_input:
+#         position = alphabet_2.index(i)
+#         new_position = position + shift_amount
+#         new_word = alphabet_2[new_position]
+#         new_cipher += new_word
+#     print(new_cipher)
+
+
+# def decrypt(shift_amount, text_input):
+#     dec_cipher = ''
+#     for i in text_input:
+#         position = alphabet_2.index(i)
+#         new_position = position - shift_amount
+#         new_word = alphabet_2[new_position]
+#         dec_cipher += new_word
+#     print(dec_cipher)
+
+
+# def method(dir):
+#     if dir == 'encode':
+#         encrypt(shift_amount=shift, text_input=text)
+#     elif dir == 'decode':
+#         decrypt(shift_amount=shift, text_input=text)
+#     else:
+#         print("wrong input")
+
+
+# method(direction)
+
+def ceasar(dir, shift_amount, text_input):
     new_cipher = ''
     for i in text_input:
         position = alphabet_2.index(i)
-        new_position = position + shift_amount
+        if dir == 'encode':
+            new_position = position + shift_amount
+        elif dir == 'decode':
+            new_position = position - shift_amount
         new_word = alphabet_2[new_position]
         new_cipher += new_word
-    print(new_cipher)
+    print(f'you just {dir}, the new cipher is {new_cipher}')
 
 
-def decrypt(shift_amount, text_input):
-    dec_cipher = ''
-    for i in text_input:
-        position = alphabet_2.index(i)
-        new_position = position - shift_amount
-        new_word = alphabet_2[new_position]
-        dec_cipher += new_word
-    print(dec_cipher)
-
-
-def method(dir):
-    if dir == 'encode':
-        encrypt(shift_amount=shift, text_input=text)
-    elif dir == 'decode':
-        decrypt(shift_amount=shift, text_input=text)
-    else:
-        print("wrong input")
-
-
-method(direction)
+ceasar(direction, shift, text)
