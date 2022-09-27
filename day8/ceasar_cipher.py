8aeaecf82d0e5b1a4c0e394f4cf33acb21ae475a
EEthanZZ <yczhu@yahoo.com>
2022-09-27 23:17:19 +1000

    decode mothod compelete

--- day8/ceasar_cipher.py
+++ day8/ceasar_cipher.py
@@ -1,11 +1,10 @@
-from turtle import pos
 
 
 alphabet_1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
 alphabet_2 = alphabet_1 + alphabet_1
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -20,4 +19,23 @@ def encrypt(shift_amount, text_input):
     print(new_cipher)
 
 
-encrypt(shift_amount=shift, text_input=text)
+def decrypt(shift_amount, text_input):
+    dec_cipher = ''
+    for i in text_input:
+        position = alphabet_2.index(i)
+        new_position = position - shift_amount
+        new_word = alphabet_2[new_position]
+        dec_cipher += new_word
+    print(dec_cipher)
+
+
+def method(dir):
+    if dir == 'encode':
+        encrypt(shift_amount=shift, text_input=text)
+    elif dir == 'decode':
+        decrypt(shift_amount=shift, text_input=text)
+    else:
+        print("wrong input")
+
+
+method(direction)
