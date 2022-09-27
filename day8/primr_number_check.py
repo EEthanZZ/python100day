c4d76fc468022d6c53dd4a6c7b0c36ef8509ce05
EEthanZZ <yczhu@yahoo.com>
2022-09-27 21:57:18 +1000

    prime number method 2

--- day8/primr_number_check.py
+++ day8/primr_number_check.py
@@ -1,16 +1,31 @@
 n = int(input("check this number:"))
 
 
-def prime_check(number):
-    a = 2
-    while a < number:
-        if number % a == 0:
-            print(f"{number} is not a prime nunmber")
-            break
-        else:
-            a += 1
-            if a == (number - 1):
-                print(f"{number} is a prime nunmber")
+# def prime_check(number):
+#     a = 2
+#     while a < number:
+#         if number % a == 0:
+#             print(f"{number} is not a prime nunmber")
+#             break
+#         else:
+#             a += 1
+#             if a == (number - 1):
+#                 print(f"{number} is a prime nunmber")
 
 
-prime_check(number=n)
+# prime_check(number=n)
+
+
+def prime_check_2(number):
+    is_Prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_Prime = False
+
+    if is_Prime:
+        print("prime number")
+    else:
+        print("not a prime number")
+
+
+prime_check_2(number=n)
