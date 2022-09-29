2af5e99c6eb1e6e0593b4afa6f667f1abf98d18a
EEthanZZ <yczhu@yahoo.com>
2022-09-29 22:36:12 +1000

    method 2, list append

--- city_visited.py
+++ city_visited.py
@@ -15,13 +15,24 @@ travel_log = [
 # to be added to the travel_log.
 
 
-def add_new_country(country, times, cities):
-    for i in range(0, len(travel_log)):
-        travel_log[i]['country'] = country
-        travel_log[i]['visits'] = times
-        travel_log[i]['cities'] = cities
+# def add_new_country(country, times, cities):
+#     for i in range(0, len(travel_log)):
+#         travel_log[i]['country'] = country
+#         travel_log[i]['visits'] = times
+#         travel_log[i]['cities'] = cities
 
 
-# 🚨 Do not change the code below
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log[-1])
+# # 🚨 Do not change the code below
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log[-1])
+
+def add_new_country2(country, times, cities):
+    new_country = {}
+    new_country['country'] = country
+    new_country['visits'] = times
+    new_country['cities'] = cities
+    travel_log.append(new_country)
+
+
+add_new_country2("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
