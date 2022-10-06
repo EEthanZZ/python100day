2f36adf326d88fc0ed9b3b2eecedac080e3b1482
EEthanZZ <yczhu@yahoo.com>
2022-10-06 14:34:16 +1100

    method in the Class

--- class_practise1.py
+++ class_practise1.py
@@ -1,15 +1,19 @@
-from curses.ascii import US
-from mimetypes import init
-
 
 class User:
     def __init__(self, userid, username):
         self.userid = userid
         self.username = username
-        self.followers = 10
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user1 = User(username='james', userid=23)
-print(user1.username)
-print(user1.userid)
-print(user1.followers)
+user2 = User(0, "Zion")
+
+user1.follow(user2)
+print(user2.followers)
+print(user1.following)
