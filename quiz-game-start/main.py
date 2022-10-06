11d7d09ff4a2e55c9c56c1c1c98b13a77d95ba5e
EEthanZZ <yczhu@yahoo.com>
2022-10-06 16:14:23 +1100

    continue the game

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -2,14 +2,23 @@ from operator import itemgetter
 from question_model import Question
 from data import question_data
 from tabulate import tabulate
-
+from quiz_brain import QuizBrain
 
 question_bank = []
 for i in question_data:
-    question_data = Question(i['text'], i['answer'])
-    question_bank.append(i)
+    ques_text = i['text']
+    ques_ans = i['answer']
+    question_data_new = Question(ques_text, ques_ans)
+    question_bank.append(question_data_new)
 
 # print(question_bank)
-header = question_bank[0].keys()
-rows = [i.values() for i in question_bank]
-print(tabulate(rows, header))
+# header = question_bank[0].keys()
+# rows = [i.values() for i in question_bank]
+# print(tabulate(rows, header))
+
+# print(tabulate(question_bank))
+
+quiz = QuizBrain(questionList=question_bank)
+
+while quiz.if_still_have_q():
+    quiz.next_q()
