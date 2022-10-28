34671a59785d1ec2bfe9c7047817098b84fed248
EEthanZZ <yczhu@yahoo.com>
2022-10-29 10:46:24 +1100

    read the weather from weather_data and print recursively using csv library

--- read&write/read_weather.py
+++ read&write/read_weather.py
@@ -1,7 +1,13 @@
-weather_list = list()
-with open("weather_data.csv") as weather:
-    weather_day = weather.readlines()
-    print(weather_day)
-    for i in weather_day:
-        i.strip("\n")
-        print(i)
+# weather_list = list()
+# with open("weather_data.csv") as weather:
+#     weather_day = weather.readlines()
+#     print(weather_day)
+#     for i in weather_day:
+#         i.strip("\n")
+#         print(i)
+
+import csv
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    for row in data:
+        print(row)
