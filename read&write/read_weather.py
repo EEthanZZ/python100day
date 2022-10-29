9530d464e8539b2ed99c0d5298e9bac36be8aa61
EEthanZZ <yczhu@yahoo.com>
2022-10-29 12:10:15 +1100

    pandas practise and export the csv

--- read&write/read_weather.py
+++ read&write/read_weather.py
@@ -18,22 +18,35 @@
 #     print(tem)
 
 import pandas
-data = pandas.read_csv("weather_data.csv")
-# print(data["day"])
-data_dict = data.to_dict()
-temp_list = data["temp"].to_list()
-ave = sum(temp_list) / len(temp_list)
-# print(ave)
+# data = pandas.read_csv("weather_data.csv")
+# # print(data["day"])
+# data_dict = data.to_dict()
+# temp_list = data["temp"].to_list()
+# ave = sum(temp_list) / len(temp_list)
+# # print(ave)
 # print(data["temp"].max())
 # print(data.day)
 # print(data["day"])
+#
+# monday = data[data.day == 'Monday']
+# print(int(monday.temp) * 3)
+#
+# data_score = {
+#     "student": ["Amy", "John", "Jason"],
+#     "scores": [10, 20, 30]
+# }
+# score = pandas.DataFrame(data_score)
+# print(score)
+# score.to_csv("new_data.csv")
 
-monday = data[data.day == 'Monday']
-print(int(monday.temp) * 3)
-
-data_score = {
-    "student": ["Amy", "John", "Jason"],
-    "scores": [10, 20, 30]
+import pandas
+data_squ = pandas.read_csv("2018_Squirrel_Data.csv")
+grey = len(data_squ[data_squ["Primary Fur Color"] == "Gray"])
+red = len(data_squ[data_squ["Primary Fur Color"] == "Cinnamon"])
+black = len(data_squ[data_squ["Primary Fur Color"] == "Black"])
+data_dict = {
+    "colour": ["grey", "red", "black"],
+    "amount": [grey, red, black],
 }
-score = pandas.DataFrame(data_score)
-print(score)
+amount = pandas.DataFrame(data_dict)
+amount.to_csv("squ.csv")
