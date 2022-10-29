1d1b31d35de7114dfc1069a479c88092596e6d92
EEthanZZ <yczhu@yahoo.com>
2022-10-29 11:21:40 +1100

    pandas practise

--- read&write/read_weather.py
+++ read&write/read_weather.py
@@ -19,4 +19,21 @@
 
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# print(data["day"])
+data_dict = data.to_dict()
+temp_list = data["temp"].to_list()
+ave = sum(temp_list) / len(temp_list)
+# print(ave)
+# print(data["temp"].max())
+# print(data.day)
+# print(data["day"])
+
+monday = data[data.day == 'Monday']
+print(int(monday.temp) * 3)
+
+data_score = {
+    "student": ["Amy", "John", "Jason"],
+    "scores": [10, 20, 30]
+}
+score = pandas.DataFrame(data_score)
+print(score)
