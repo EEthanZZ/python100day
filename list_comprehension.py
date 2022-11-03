7d73ddc7e022efa543daffd65905b0484c94e6d0
EEthanZZ <yczhu@yahoo.com>
2022-11-03 23:59:14 +1100

    list comprehension with if statement

--- list_comprehension.py
+++ list_comprehension.py
@@ -15,3 +15,9 @@ print(new_name)
 range_1 = range(1,5)
 range_2 = [n*2 for n in range_1]
 print(range_2)
+
+name_list = ["Joseph", "Michael", "Manning", "Joe", "An"]
+new_name_list = [i[:3]for i in name_list]
+new_name_list2 = [i for i in name_list if len(i) < 4]
+print(new_name_list)
+print(new_name_list2)
