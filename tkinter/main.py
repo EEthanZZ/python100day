1fb5a992458250350c26d26cf81b4823194ff551
EEthanZZ <yczhu@yahoo.com>
2022-11-05 22:35:32 +1100

    example of keyward advanced arguments/*args

--- tkinter/main.py
+++ tkinter/main.py
@@ -4,7 +4,6 @@ window = tkinter.Tk()
 window.title("Window 1")
 window.minsize(width=500, height=300)
 
-
 my_label = tkinter.Label(text="label 1", font=("Arial", 24, "bold"))
 my_label.pack(side="left")
 
@@ -13,18 +12,18 @@ def add(*args):
     sum = 0
     for i in args:
         sum += i
-    print(sum)
-
-add(3,4,5)
+    return sum
 
 
+print(add(3, 4, 5))
 
 
+def cal(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["mul"]
+    print(n)
 
 
-
-
-
-
+cal(n=3, add=2, mul=5)
 
 window.mainloop()
