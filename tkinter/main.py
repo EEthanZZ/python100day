9055dda242c912100f55b4bfb99e9d2cb0867b9c
EEthanZZ <yczhu@yahoo.com>
2022-11-05 23:15:05 +1100

    tkinter button function

--- tkinter/main.py
+++ tkinter/main.py
@@ -5,7 +5,19 @@ window.title("Window 1")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="label 1", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label.pack(side="top")
+
+my_label.config(text="YYYYYY")
+my_label["text"] = "XXXXXX"
+
+
+def button_click():
+    pass
+
+
+button = tkinter.Button(text="button")
+button.pack()
+
 
 
 def add(*args):
@@ -29,11 +41,11 @@ cal(n=3, add=2, mul=5)
 
 class Car:
     def __init__(self, **kwargs):
-        self.model = kwargs["make"]
-        self.make = kwargs["model"]
+        self.model = kwargs.get("model")
+        self.make = kwargs.get("make")
 
 
-my_car = Car(make="Toyota", model="Rav4")
-print(my_car.model)
+my_car = Car(make="1010", model="ccc")
+print(my_car.make)
 
 window.mainloop()
