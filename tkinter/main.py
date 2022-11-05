74a31ca84f0d53a5be9e14e71fa60184bbe1f40d
EEthanZZ <yczhu@yahoo.com>
2022-11-05 22:23:14 +1100

    example of advanced arguments/*args

--- tkinter/main.py
+++ tkinter/main.py
@@ -6,8 +6,16 @@ window.minsize(width=500, height=300)
 
 
 my_label = tkinter.Label(text="label 1", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.pack(side="left")
 
+
+def add(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    print(sum)
+
+add(3,4,5)
 
 
 
