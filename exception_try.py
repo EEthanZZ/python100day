f1b31e9269e97c963e636ecf8923ef56f9076119
EEthanZZ <yczhu@yahoo.com>
2022-11-12 22:56:30 +1100

    practise, KeyError

--- exception_try.py
+++ exception_try.py
@@ -27,14 +27,35 @@
 fruits = ["Apple", "Pear", "Orange"]
 
 
-def make_pie(index):
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError as error:
+#         print("fruit pie")
+#     else:
+#         print(fruit + " pie")
+#
+#
+# make_pie(2)
+# make_pie(4)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
     try:
-        fruit = fruits[index]
-    except IndexError as error:
-        print("fruit pie")
-    else:
-        print(fruit + " pie")
+        total_likes = total_likes + post['Likes']
+    except KeyError as error:
+        total_likes += 0
 
 
-make_pie(2)
-make_pie(4)
+
+print(total_likes)
