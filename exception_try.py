362e5ebd389532c3193fe48da01842a7eda70d7e
EEthanZZ <yczhu@yahoo.com>
2022-11-12 22:28:06 +1100

    else & finally

--- exception_try.py
+++ exception_try.py
@@ -1,8 +1,14 @@
 try:
-    file = open("afile.txt")
-    diction = {"key":"value"}
-    print(diction["dassa"])
+    file = open("./afile.txt")
+    diction = {"key": "value"}
+    print(diction["key"])
 except FileNotFoundError:
-    open("afile.txt", mode="w")
+    file = open("./afile.txt", mode="a")
+    file.write("hi")
 except KeyError as error:
     print(f"the key {error} does;t exist")
+else:
+    x = file.read()
+    print(x)
+finally:
+    file.close()
