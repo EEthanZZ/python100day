de4571b272cc508ba10b1b698b4c479cc62edc66
EEthanZZ <yczhu@yahoo.com>
2022-11-12 22:43:53 +1100

    make your own error example

--- exception_try.py
+++ exception_try.py
@@ -1,15 +1,23 @@
-try:
-    file = open("./afile.txt")
-    diction = {"key": "value"}
-    print(diction["key"])
-except FileNotFoundError:
-    file = open("./afile.txt", mode="a")
-    file.write("hi")
-except KeyError as error:
-    print(f"the key {error} does;t exist")
-else:
-    x = file.read()
-    print(x)
-finally:
-    file.close()
-    raise KeyError("my error")
+# try:
+#     file = open("./afile.txt")
+#     diction = {"key": "value"}
+#     print(diction["key"])
+# except FileNotFoundError:
+#     file = open("./afile.txt", mode="a")
+#     file.write("hi")
+# except KeyError as error:
+#     print(f"the key {error} does;t exist")
+# else:
+#     x = file.read()
+#     print(x)
+# finally:
+#     file.close()
+#     raise KeyError("my error")
+
+
+height = float(input("Height:"))
+weight = float(input("Weight:"))
+
+if height > 3:
+    raise ValueError("not a true value")
+bmi = height / weight ** 2
